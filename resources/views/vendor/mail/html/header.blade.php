9a1945b6f68440a0e8aa97c525eaea40a22bba2a
Fleksis <markuss0303@gmail.com>
2023-01-05 22:39:20 +0200

    feat: mail commit for server

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,10 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="https://api.brain-cloud.uk" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://api.brain-cloud.uk/Logo.png" class="logo">
+    <h1 style="font-size: 72px; padding-left: 20px; padding-right: 20px">X</h1>
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @endif
 </a>
