34d48eb7d9df6fa494595827ac7cd401df591f07
Fleksis <markuss0303@gmail.com>
2023-01-05 22:50:58 +0200

    feat: mail commit for server test

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,9 +4,17 @@
 <a href="https://api.brain-cloud.uk" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://api.brain-cloud.uk/Logo.png" class="logo">
-    <h1 style="font-size: 72px; padding-left: 20px; padding-right: 20px">X</h1>
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @endif
 </a>
 </td>
 </tr>
+
+<style>
+    .between-x {
+        font-size: 72px;
+        padding-left: 20px;
+        padding-right: 20px;
+        margin: 0;
+        font-weight: lighter;
+    }
+</style>
